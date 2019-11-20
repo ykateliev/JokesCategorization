5a70a86976f7a069ebd124108e54ce439717a085
yane-PC\yane <yane.kateliev@gmail.com>
2019-11-20 22:13:54 +0200

    Add Project

---
+++ JokesCategorization/Controllers/DataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JokesCategorization.Controllers
+{
+	public class DataController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> GetData()
+        {
+
+
+			return View();
+		}
+	}
+}
