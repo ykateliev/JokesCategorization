fb112da1990cefeb1df73afc982778f9e04d352b
yane-PC\yane <yane.kateliev@gmail.com>
2019-11-23 16:53:41 +0200

    Optimiza Data Gatherer.

--- JokesCategorizationBG/DataGatherer.cs
+++ JokesCategorizationBG/DataGatherer.cs
@@ -1,46 +1,46 @@
-﻿using AngleSharp;
+﻿using AngleSharp.Html.Parser;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
+using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace JokesCategorizationBG
 {
     public class DataGatherer
     {
-        public async Task<List<JokeModel>> GatherData(int startJokeId, int endJokeId)
+        public List<JokeModel> GatherData(int startJokeId, int endJokeId)
         {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var webClient = new WebClient { Encoding = Encoding.GetEncoding("windows-1251") };
+            var parser = new HtmlParser();
             var jokes = new List<JokeModel>();
-            var config = Configuration.Default.WithDefaultLoader();
-            var context = BrowsingContext.New(config);
 
             for (int i = startJokeId; i < endJokeId; i++)
             {
                 try
                 {
-                    var document = await context.OpenAsync("https://fun.dir.bg/vic_open.php?id=" + i);
-
-                    var text = document.QuerySelectorAll("#newsbody");
+                    var document = webClient.DownloadString("https://fun.dir.bg/vic_open.php?id=" + i);
+                    var parsedDocument = parser.ParseDocument(document);
+                    var text = parsedDocument.QuerySelectorAll("#newsbody");
+                    var category = parsedDocument.QuerySelectorAll(".tag-links-left>a");
 
                     if (text.Length > 0)
                     {
                         var jokeText = text[0]
                         .TextContent
-                        .Replace("\n", string.Empty)
-                        .Replace("\t", string.Empty)
-                        .Replace('"', '\'')
                         .TrimStart()
                         .Trim();
 
-                        var category = document.QuerySelectorAll(".tag-links-left>a");
+                        jokeText = Regex.Replace(jokeText, "[\n|\t]+", string.Empty);
+                        jokeText = Regex.Replace(jokeText, "['|\"|.|,|,|+|-|?|!|:]+", " ");
+
                         var jokeCategory = category?[0].TextContent;
 
-                        if (!string.IsNullOrWhiteSpace(jokeCategory) && jokeCategory.ToLower() != "разни")
+                        if (!string.IsNullOrWhiteSpace(jokeCategory))
                         {
                             jokes.Add(new JokeModel { Text = jokeText, Category = jokeCategory });
-                            Console.WriteLine($"Add Joke with Id:{i}");
+                            Console.WriteLine($"Add Joke with id:{i}");
                         }
                     }
                 }
@@ -49,6 +49,7 @@ namespace JokesCategorizationBG
                     continue;
                 }
             }
+
 
             return jokes;
         }
