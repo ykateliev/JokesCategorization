455c94e6a2e486b442e6fe209a652d8167b86374
yane-PC\yane <yane.kateliev@gmail.com>
2019-11-23 16:54:54 +0200

    Get data file.

--- JokesCategorizationBG/Program.cs
+++ JokesCategorizationBG/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace JokesCategorizationBG
@@ -7,21 +8,28 @@ namespace JokesCategorizationBG
     {
         public static void Main(string[] args)
         {
-            var jokes = new DataGatherer().GatherData(1, 50000).Result;
+            var jokes = new DataGatherer().GatherData(1, 48000);
+            var cleaner = new DataCleaner();
+            cleaner.RemoveJokesFromCategoryRazni(jokes);
+            cleaner.GetJokesMoreThan20InCategory(jokes);
+            
+
+            var dataFile = "data.csv";
 
             Console.WriteLine("---- Creating data file. ----");
             Console.WriteLine($"{jokes.Count} jokes.");
 
-            using (var writer = new StreamWriter("data.csv"))
+            using (var writer = new StreamWriter(dataFile))
             {
-                writer.WriteLine("text, category");
+                writer.WriteLine("category, text");
                 foreach (var joke in jokes)
                 {
-                    writer.WriteLine(string.Join(',', joke.Text, joke.Category));
+                    writer.WriteLine($"{joke.Category}, \"{joke.Text}\"");
                 }
             }
 
             Console.WriteLine("---- Data file is ready. ----");
+            Console.WriteLine("done");
         }
     }
 }
