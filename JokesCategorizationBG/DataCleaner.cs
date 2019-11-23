ec6d3a76ea533b15a647fc3678cb46d2bcc6aef2
yane-PC\yane <yane.kateliev@gmail.com>
2019-11-23 16:54:13 +0200

    Add Data Cleaner.

---
+++ JokesCategorizationBG/DataCleaner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JokesCategorizationBG
+{
+    public class DataCleaner
+    {
+        public List<JokeModel> GetJokesMoreThan20InCategory(List<JokeModel> jokes)
+        {
+            var categories = jokes.Select(jm => jm.Category).Distinct().ToList();
+
+            foreach (var category in categories)
+            {
+                var jokesFromCategory = from joke in jokes
+                                        where joke.Category == category
+                                        select joke;
+
+                if (jokesFromCategory.ToList().Count < 2)
+                {
+                    jokes.RemoveAll(x => x.Category == category);
+                }
+            }
+
+            return jokes;
+        }
+
+        public List<JokeModel> RemoveJokesFromCategoryRazni(List<JokeModel> jokes)
+        {
+            jokes.RemoveAll(x => x.Category.ToLower() == "разни");
+            return jokes;
+        }
+    }
+}
